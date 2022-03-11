88af0fdca575a34528470ffb31c703833cceaa95
chris01-code <chrismorara@gmail.com>
2022-03-11 14:45:49 +0000

    Size of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	char c;
 	float f;
 
-	printf("size of a char %lu byte(s)\n", (unsigned long)sizeof(c));
-	printf("size of an int %lu byte(s)\n", (unsigned long)sizeof(i));
-	printf("size of a long int %lu byte(s)\n", (unsigned long)sizeof(sizeof1));
-	printf("size of a long long %lu byte(s)\n", (unsigned long)sizeof(sizeof2));
-	printf("size of a float %lu byte(s)\n", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(sizeof1));
+	printf("Size of a long long: %lu byte(s)\n", (unsigned long)sizeof(sizeof2));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
