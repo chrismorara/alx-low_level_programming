9ccf48ed2a3d893bfce027d22e298a0e822ff69d
chris01-code <chrismorara@gmail.com>
2022-03-11 08:50:33 +0000

    Size of program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	int i;
+	long int sizeof1;
+	long long int sizeof2;
+	char c;
+	float f;
+
+	printf("sizeof int %lu byte(s)\n", (unsigned long)sizeof(i));
+	printf("sizeof long int %lu byte(s)\n", (unsigned long)sizeof(sizeof1));
+	printf("sizeof long long int %lu byte(s)\n", (unsigned long)sizeof(sizeof2));
+	printf("sizeof char %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("sizeof float %lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
