64f7ae1b9b05743164fde0b707b983cf8e5ea936
chris01-code <chrismorara@gmail.com>
2022-03-11 09:27:49 +0000

    Size of program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,14 @@
 int main(void)
 {	int i;
 	long int sizeof1;
-	long long int sizeof2;
+	long long sizeof2;
 	char c;
 	float f;
 
 	printf("sizeof char %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("sizeof int %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("sizeof long int %lu byte(s)\n", (unsigned long)sizeof(sizeof1));
-	printf("sizeof long long int %lu byte(s)\n", (unsigned long)sizeof(sizeof2));
+	printf("sizeof long long %lu byte(s)\n", (unsigned long)sizeof(sizeof2));
 	printf("sizeof float %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
 }
