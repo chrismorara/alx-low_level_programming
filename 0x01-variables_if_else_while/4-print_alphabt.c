7e3e67609c127063bf12d3c82bbde867f982ab11
chris01-code <chrismorara@gmail.com>
2022-03-13 22:30:32 +0000

    lowercase

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	char ch = 'a';
-		while(ch <= 'z')
+		while (ch <= 'z')
 		{
 			if (ch != 'e' && ch != 'q')
 			{
@@ -16,7 +16,6 @@ int main(void)
 			ch++;
 		}
 		putchar('\n');
-	
-	return 0;
+	return (0);
 }
 
