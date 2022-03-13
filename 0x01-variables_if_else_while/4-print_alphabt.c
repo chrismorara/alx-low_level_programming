dc12cbb16ee8735c52f1f2869e7e22cc6c19bde5
chris01-code <chrismorara@gmail.com>
2022-03-13 22:26:20 +0000

    lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Program entry point
+ *
+ * Return: 0 on success. Error code otherwise
+ */
+int main(void)
+{
+	char ch = 'a';
+		while(ch <= 'z')
+		{
+			if (ch != 'e' && ch != 'q')
+			{
+				putchar(ch);
+			}
+			ch++;
+		}
+		putchar('\n');
+	
+	return 0;
+}
+
