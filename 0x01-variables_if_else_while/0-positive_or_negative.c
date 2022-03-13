79734a066cc8ab0778b72b2ba0d2835f58d469ed
chris01-code <chrismorara@gmail.com>
2022-03-13 20:30:33 +0000

    Random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,21 +5,17 @@
 /**
 * main - entry point
 *
-* Return: Always 0 (Success):wq
+* Return: Always 0 (Success):wiq
 */
 int main(void)
-{	
-	int n;	
-	
+{int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
 		printf("%d is zero\n", n);
 	else
 		printf("%d is negative\n", n);
-		
 	return (0);
 }
