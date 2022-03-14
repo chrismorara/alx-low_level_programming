fb70c2c7e4a727ff7530baec85051ae8c217675e
chris01-code <chrismorara@gmail.com>
2022-03-14 16:06:05 +0000

    Combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{	int i, j;
+	for (i = 0; i <= 9; i++)
+
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			if (((i * 10) + j) < 89)
+			continue;
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
