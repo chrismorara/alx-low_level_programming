4563a16d9c8b9782369d0535146bd8b60e1060fa
chris01-code <chrismorara@gmail.com>
2022-03-14 06:38:46 +0000

    alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char _back;
+
+	for (_back = 'z'; _back >= 'a'; _back--)
+	putchar(_back);
+	putchar('\n');
+
+	return (0);
+}
