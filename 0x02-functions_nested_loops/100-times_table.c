f7b129374b465b6ec71fc2f3aac6d22d16ad99bf
Christopher Morara <chrismorara@gmail.com>
2022-03-16 15:37:55 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,34 +9,41 @@
 void print_times_table(int n)
 {
 	int a, b, p = 0;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
 		{
 			for (b = 0; b <= n; b++)
 			{
-			p = a * b;
-			if (b > 0)
-			{
-				_putchar(',');
-				_putchar(' ');
+				p = a * b;
+				if (b > 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (p < 10)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + p);
+					}
+					else if (p >= 10 && p < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (p / 10));
+						_putchar('0' + (p % 10));
+					}
+					else if (p >= 100)
+					{
+						_putchar('0' + (p / 100));
+						_putchar('0' + ((p / 10) % 10));
+						_putchar('0' + (p % 10));
+					}
+				}
+				else
+					_putchar('0' + p);
 			}
-			if (p < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + p);
-			}
-			else if (p >= 10 && p < 100)
-			{
-				_putchar('0' + (p / 100));
-				_putchar('0' + (p / 100) %10);
-				_putchar('0' + (p %10));
-			}
-			}
-			else
-				_putchar('0' + p);
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
