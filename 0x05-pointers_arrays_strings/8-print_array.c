b309009f0bf01ee2871520b58d4c7ac3a11b7f20
chris01-code <chrismorara@gmail.com>
2022-03-22 16:58:29 -0700

    Print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print n of int array followed by a new line
+ * @a: pointer
+ * @n: n
+ */
+void print_array(int *a, int n)
+{
+	short z = 0;
+
+	while (n-- > 0)
+	{
+		printf("%d", a[z++]);
+		if (n != 0)
+			printf(", ");
+	}
+	printf("\n");
+}
