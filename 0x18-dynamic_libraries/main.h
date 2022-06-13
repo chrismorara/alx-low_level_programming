2a99967adf7cf68565e9fb99aaf20f6e2200c607
Christopher Morara <chrismorara@gmail.com>
2022-06-14 01:56:15 +0300

    Update main.h

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -24,10 +24,9 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-int main(void);
-int add(void);
-int sub(void);
-int division(void);
-int mul(void);
-int modulo(void);
+int mod(int a, int b);
+int div(int a, int b);
+int mul(int a, int b);
+int sub(int a, int b);
+int add(int a, int b);
 #endif
