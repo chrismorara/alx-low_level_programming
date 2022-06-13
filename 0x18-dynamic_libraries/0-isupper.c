29a5b92dc3b36128c7c9e90b73ae21c94709b2be
Christopher Morara <chrismorara@gmail.com>
2022-06-13 23:52:54 +0300

    Update 0-isupper.c

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _isupper - check for uppercase
+  * @c: parameter
+  * Return: 0
+  */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
